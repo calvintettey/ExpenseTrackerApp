04ded8f75e883159bb6326a790d7de4669b4f138
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-05-01 10:30:44 +0000

    built navbar section

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { COLORS, FONTS, SIZES, icons} from '../constants'
-
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { COLORS, FONTS, SIZES, icons } from "../constants";
 
 export default function Home() {
+  function renderNavBar() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
+      <View
+      style={{
+          flexDirection: "row",
+          height: 80,
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+          paddingHorizontal: SIZES.padding,
+          backgroundColor: COLORS.white,
+      }}>
+        <TouchableOpacity
+            style={{justifyContent:"center",width: 50 }}
+            onPress={() => console.log("Back")}
+        >
+          <Image
+            source={icons.back_arrow}
+            style={{
+              width: 30,
+              height: 30,
+              tintColor: COLORS.primary,
+            }}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        style={{justifyContent:"center", alignItems: 'flex-end', width: 50 }}
+            onPress={() => console.log("More")}>
+          <Image
+            source={icons.more}
+            style={{
+              width: 30,
+              height: 30,
+              tintColor: COLORS.primary,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: COLORS.lightGray2 }}>
+      {/* Navbar section */}
+      {renderNavBar()}
+      
+    </View>
+  );
 }
