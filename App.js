9d8e024dfd89659e7ea4db5c5c2623732b5edd2f
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-05-01 10:30:21 +0000

    imported createStackNavigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {
   DefaultTheme,
   NavigationContainer,
 } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
