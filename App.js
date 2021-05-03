36fb0c469baed7bdfbd68a6a5a8984dae1b26222
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-05-03 11:39:38 +0000

    remove redundant code

--- App.js
+++ App.js
@@ -3,9 +3,7 @@ import {
   NavigationContainer,
 } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Home } from "./screens/";
 
 const theme = {
